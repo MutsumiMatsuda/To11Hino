6d03a0539af8f8b6c93d7de2616cb6cc93596da7
MutsumiMatsuda <matsuda_mutsumi@r3.dion.ne.jp>
2018-12-18 17:01:06 +0900

    Create user.rb

---
+++ user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable#, :confirmable
+
+  attr_accessor :current_password
+
+  # 8~32文字内で半角英数字が1つ以上含まれている
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
+#松田変更ここから
+#  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, on: :update, allow_blank: true
+#松田変更ここまで
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :user_name, presence: true, uniqueness: true
+
+  mount_uploader :image, ImageUploader
+
+    def update_without_current_password(params, *options)
+      # edit機能を使うためにユーザーのパスワードを入力するというdeviseの使用を変更するため
+      params.delete(:current_password)
+
+      # パスワード、確認パスワードが両方入力されているときのみ更新できるようにするため
+      if params[:password].blank? && params[:password_confirmation].blank?
+        params.delete(:password)
+        params.delete(:password_confirmation)
+      end
+
+      result = update_attributes(params, *options)
+      clean_up_passwords
+      result
+    end
+end
